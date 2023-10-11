8359642ea35b8097e7f5b00f265acca96ed0075e
PC-002\Carlos <carloscf.correo@gmail.com>
2023-10-11 19:16:24 +0200

    Console project

---
+++ FourInLine/FourInLine/Game/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace FourInLine.Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+        }
+    }
+}
