2ce1b5736d3e6d37fca455377f2aa038c347cb99
PC-002\Carlos <carloscf.correo@gmail.com>
2023-10-25 17:41:48 +0200

    Init NegaScout

---
+++ FourInLine/FourInLine/AI/NegaScout.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourInLine.AI
+{
+    public class NegaScout
+    {
+
+    }
+}
