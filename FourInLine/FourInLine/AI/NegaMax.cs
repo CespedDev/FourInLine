76330e52a7a0405370c2601b66fe0b96d30ada17
superaxel13 <72009213+superaxel13@users.noreply.github.com>
2023-12-22 19:07:04 +0100

    NegaMax

--- FourInLine/FourInLine/AI/NegaMax.cs
+++ FourInLine/FourInLine/AI/NegaMax.cs
@@ -13,8 +13,7 @@ namespace FourInLine.AI
 
         public int MakeDecision(Board board)
         {
-            int alpha = int.MinValue;
-            int beta = int.MaxValue;
+        
             int bestColum = -1; //Initialize with an invalid colum
 
             //Initialize bestScore to a very low value
@@ -25,7 +24,7 @@ namespace FourInLine.AI
                 Board newBoard = new Board(board, col);
 
                 //Recurse.
-                int recursedScore = -NegaMaxAB(new Board(newBoard), depth, -beta, -alpha);
+                int recursedScore = NegaMax_(new Board(newBoard), depth);
                 int currentScore = -recursedScore;
 
                 //Update the best score and colum
@@ -35,18 +34,11 @@ namespace FourInLine.AI
                     bestColum = col;
                 }
 
-                //Update the alpha value
-                alpha = Math.Max(alpha, bestScore);
-
-                //Check for pruning
-                if (alpha >= beta)
-                    break;
-
             }
             return bestColum;
         }
 
-        private int NegaMaxAB(Board board, int maxDepht, int alpha, int beta, int currentDepth = 0)
+        private int NegaMax_(Board board, int maxDepht, int currentDepth = 0)
         {
             //Check if we're done recursing
             if (board.IsGameOver() || currentDepth == maxDepht)
@@ -55,46 +47,21 @@ namespace FourInLine.AI
             //Otherwise bubble up values from below
             int bestScore = int.MinValue;
 
-            //Keep track of the Test window value
-            int adaptiveBeta = beta;
-
             //Go through each move
             foreach(int col in board.PosiblesInsert())
             {
                 Board newBoard = new Board(board, col);
                 //Recurse
-                int recursedScore = -NegaMaxAB(new Board(newBoard), maxDepht, -adaptiveBeta, -Math.Max(alpha, bestScore), currentDepth + 1);
+                int recursedScore = NegaMax_(new Board(newBoard), maxDepht, currentDepth + 1);
                 int currentScore = -recursedScore;
             
                 //Update the best score
                 if(currentScore>bestScore)
-                {
-                    //if we are in 'narrow-mode' then widen and do a regular
-                    //AB negamax search
-                    if(adaptiveBeta==beta||currentDepth>=maxDepht-2)
-                    {
-                        bestScore = currentScore;
-                    }
-                    //Otherwise, we can do a Test
-                    else
-                    {
-                        int negativeBestScore = -NegaMaxAB(new Board(newBoard), maxDepht, -beta, -currentScore, currentDepth);
-                        bestScore = -negativeBestScore;
-                    }
-
-                    //if we're outside the bound, prune by exiting
-                    if(bestScore >=beta)
-                    {
-                        return bestScore;
-                    }
-
-                    //Otherwise update the window location
-                    adaptiveBeta = Math.Max(alpha, bestScore) + 1;
+                {   
+                        bestScore = currentScore;                           
                 }
             }
             return bestScore;
         }
-
-
     }
 }
